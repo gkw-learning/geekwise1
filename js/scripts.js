27c229b7f6edb1c0a8bcf3333112cbd59eca0ee4
howdycrystalm <howdycrystalm@gmail.com>
2018-04-09 20:58:09 -0700

    end of day 1

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var myHistory =
+  'My name is Crystal, I want to master ES6, RESTful APIs, and CRUD operations.  Ive had to quickly learn React and Aurelia, and I realized I need to better understand ES6.';
+
+var userAnswer = confirm(myHistory);
+
+console.log(userAnswer);
