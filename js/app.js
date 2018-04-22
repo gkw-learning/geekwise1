0b096c69cc47222023493a62b4b25121969b163a
howdycrystalm <howdycrystalm@gmail.com>
2018-04-22 09:38:08 -0700

    resolve color puzzle

--- js/app.js
+++ js/app.js
@@ -1,26 +1,5 @@
-let userAge = prompt(
-  'I have to ask your age using the Date Object method. What is your birthdate (mm/dd/yyyy)?'
-);
-
-let makeDate = new Date(userAge);
-//doing a math operation converts to number because loosely typed
-let yearsOld = (new Date() - makeDate) / 31536000000;
-
-console.log(typeof userAge);
-console.log(typeof makeDate);
-console.log(typeof yearsOld);
-console.log(yearsOld);
-
-if (yearsOld < 80 && yearsOld > 18) {
-  alert('Ah, under 80. Not quite a super senior, but getting there.');
-  let userOpinion = prompt('do you like the star wars? yes or no.');
-  if (userOpinion === 'yes') {
-    alert('nerd');
-  } else {
-    alert('unpopular opinion');
-  }
-} else if (yearsOld < 18) {
-  alert(`only ${yearsOld} years old? SCRAM!`);
-} else {
-  alert(`good on you for surving ${yearsOld} years.`);
-}
+//Prompts your user for their name.
+//Next, ask the user's favorite color. (Provide the user with the basic 10 colors as options
+//i.e. red, orange, yellow, green, blue, pink, purple, black, white, and grey).
+//Depending on the color, write 9 different responses for the possible answers.
+//Include the user's name in the response message.
