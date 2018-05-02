0b71c716d17a94ffde83b4de48a71a932266c6a7
howdycrystalm <howdycrystalm@gmail.com>
2018-05-01 22:50:05 -0700

    halfway there. ohhhh. living on a...

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,3 @@
-// ***** Notes *****//
-// Querying the DOM
-//
-// let blueDivs = document.getElementsByClassName('blue'); //comes back as array. ELEMENTS will always be an array
-// console.log(blueDivs[0]);
-// //getElementsByClassName
-// //always give all elements
-// //always gives back an array]);
-//
-// let h1 = document.getElementsByTagName('h1'); //
-// console.log(h1[0].clientTop);
-//
-// let para = document.getElementById('para'); //only returns id, rember id is for javascript. should stop using it to style
-//
-// let queryBlues = document.querySelectorAll('div'); //go back and checkout  querySelector and querySelectorAll
-// console.log(queryBlues);
-//
-// /*
-// Let's try it out!
-//
-// Create a function that queries the DOM for the body tag using all of these methods. Log the results to your console.
-// The function should take 2 arguments: the DOM selector method the user wants to employ and the name of the DOM selector to be searched for.
-// */
-//
-// function findBody() {
-//   let body = document.getElementsByTagName('body');
-//   console.log(body[0]); //
-//   let body = document.getElementsByClassName('bodyClass');
-//   console.log((body[0]));
-//   let bodyId = document.getElementById('bodyId')
-// }
-//
-// findBody();
-//
-// function queryDom(element, method) {
-//   let elementToQuery = document.method(element);
-//   console.log(elementToQuery);
-// }
-//
-// let el = prompt('give me an element');
-// let method - prompt('give me a method');
-// queryDom(el, method);
-// event listeners
-// event listeners allow you to perform certain functionality only when user performs specific action
-// first class functions. this means functions can be given as a paramter, what is known as a
-// CALLBACK FUNCTION!!!!
-//
-// const button = document.querySelector('btn-danger');
-// //first argument is always the string name of the event you want to listen for
-// //the second argument is going to be a callback function
-// //CALLBACK FUNCTION is what happens when the whatever is clicked
-// //callback functions are run asynchronous
-// button.addEventListener('click', e => {
-//   console.log(e.target);
-// });
-
-/*
-Let's Try It!
-On your day6 branch, create a basic HTML mockup with a button, a div with some height and background color, and an h1 with some text
-Create event listeners for each:
-For the button, alert the user with a message when they click it.
-For the div, change the color of the background when their mouse goes into the div.
-For the h1, change the color of the text the user releases their mouse.
-Not Tough Enough? Try redirecting the user's browser to facebook when the click anywhere on the page.
-*/
-
-const button = document.querySelector('button');
-const container = document.querySelector('.container');
-const h1 = document.querySelctor('h1');
-
-button.addEventListener('click', e => {
-  alert('thanks for the click');
-});
-
-container.addEventListener('mouseenter', e => {
-  div.className = 'rebeccapurple';
-});
-
-h1.addEventListener('mouseup', e => {
-  h1.style.color = 'yellow'; //same thing as div.className, but this is better
-});
-
 //RYEKER'S NOTES FROM DAY 7
 // const div = document.querySelector('div');
 // const p = document.querySelector('p');
@@ -96,3 +14,62 @@ h1.addEventListener('mouseup', e => {
 //   newDiv.innerHTML = '<h3 class="text-center">Whoa! You clicked the thing!</h3><p>Some other things you might be interested in....</p>'; //Add some HTML content to the DIV you made
 //   div.insertBefore(newDiv, p); //Add you new DIV to a parent (const div), BEFORE a sibling node (const p).
 // });
+
+/*Write a program that converts temperatures. It should:
+
+Ask your user to give you a scale (Celcius or Fahrenheit) and the tempurature they want to convert. So if they give you 90 Fahrenheit, they would want you to convert 90 degrees Fahrenheit to whatever that is in Celcius
+Employ a function that accepts those two values as arguments: temperature and scale (either celcius or fahrenheit) and converts the tempurature they gave you to the opposite scale.
+Display the temperature in an h3 with minimal styling.
+From that point on, when the user clicks on the h3, run the function to convert the temperature back and forth between Celcius and Fahrenheit.
+Display the converted temperature in the h3 each time it is changed. */
+
+let gimmeTemp = prompt(
+  'give me a temperature to convert for you in either Fahrenheit or Celcius.'
+).split(' ');
+console.log(typeof gimmeTemp); //object = array
+console.log(gimmeTemp); // ["89", "f"]
+
+let scale = gimmeTemp.splice(1, 1).toString(); //array, to string?
+let numifyTemp = parseInt(gimmeTemp); //number
+
+console.log(`should only get back 89 ${gimmeTemp}`); //["89"]
+console.log(`should only be f ${scale}`); //["f"]
+console.log(`scale should be a ${typeof scale}`);
+console.log(typeof numifyTemp);
+//so split answer using ' ', and should split what they give me by words
+
+let cToF;
+let theH3 = theH3.getElementsByTagName('h3');
+
+function maths(argTemp, argScale) {
+  if (
+    argScale === 'f' ||
+    argScale === 'F' ||
+    argScale === 'fahrenheit' ||
+    argScale === 'Fahrenheit'
+  ) {
+    fToC = numifyTemp * 9 / 5 + 32; //converting from f to c
+    console.log(`this should give back a conversion ${fToC}`);
+    theH3.addEventListener('click', function(e) {});
+  } else if (
+    argScale === 'c' ||
+    argScale === 'C' ||
+    argScale === 'celcius' ||
+    argScale === 'Celcius'
+  ) {
+    cToF = (numifyTemp - 32) * 5 / 9; //converting from c to f
+    console.log(`this should convert to f ${cToF}`);
+  } else {
+    alert(
+      'what planet is that temperature on?  I only understand Fahrenheit or Celcius.'
+    );
+  }
+}
+console.log(typeof cToF);
+maths(numifyTemp, scale);
+//loop through answer find F or C, and give to var a
+//loop through answer and find number and give to var b
+//then give those vars as parametersfunction (Num, Scale)
+//then do if statement, and math. so if they give C, convert to f
+//after the math grab the document stuff and change the h3.
+//return function
