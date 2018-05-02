5e8978acfb4f142ff9d0ae535182c00c4cd74e84
howdycrystalm <howdycrystalm@gmail.com>
2018-05-01 20:45:26 -0700

    added ryeker's notes for future reference

--- js/scripts.js
+++ js/scripts.js
@@ -80,4 +80,19 @@ h1.addEventListener('mouseup', e => {
   h1.style.color = 'yellow'; //same thing as div.className, but this is better
 });
 
-//DAY 7 NOTES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//RYEKER'S NOTES FROM DAY 7
+// const div = document.querySelector('div');
+// const p = document.querySelector('p');
+// const button = document.querySelector('.btn-default');
+//
+// div.addEventListener('mouseover', (e) => {
+//   let newPTag = document.createElement('p'); //Create a new element
+//   newPTag.textContent = "Hey there, guy/gal/friend/frienemy!!!"; //add some text content to that element
+//   div.appendChild(newPTag); //add that element to another parent element
+// });
+//
+// button.addEventListener('click', (e) => {
+//   let newDiv = document.createElement('div'); //create a DIV
+//   newDiv.innerHTML = '<h3 class="text-center">Whoa! You clicked the thing!</h3><p>Some other things you might be interested in....</p>'; //Add some HTML content to the DIV you made
+//   div.insertBefore(newDiv, p); //Add you new DIV to a parent (const div), BEFORE a sibling node (const p).
+// });
