8aed4de0045a93422377547bd191fee515380cc0
howdycrystalm <howdycrystalm@gmail.com>
2018-05-01 20:34:56 -0700

    end of notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,110 +1,83 @@
-//***** Notes *****//
-//javascript is considered loosely typed
-//6 data types: "String", Number, Boolean, Undefined, Null
-//input fields always return a string.
-//eg when user types a 22 number in the input field, it will be converted to a '22' string
-//primitive string is only a sentance, and cant be change or manipulated by js.
-//object string ""
-//how to tell if something is primitive
-var myName = 'Crystal';
-console.log(typeof myName);
-//consloe.log will print "string". the lowercase "s" means its primitive
-//now let's force a object String
-var myName = 'Crystal';
-myName = new String(myName);
-console.log(typeof myName);
-//console will pring "object"
-//a third way to tell type of
-var myName = 'Crystal';
-myName = new String(myName);
-console.log(myName);
-//console will print an object
+// ***** Notes *****//
+// Querying the DOM
 //
-//console will use the method, toUpperCase()
-var myName = 'Crystal';
-myName = new String(myName);
-console.log(myName.toUpperCase()); //the method changes the object back into a primitive
+// let blueDivs = document.getElementsByClassName('blue'); //comes back as array. ELEMENTS will always be an array
+// console.log(blueDivs[0]);
+// //getElementsByClassName
+// //always give all elements
+// //always gives back an array]);
 //
-var myName = 1034;
-console.log(myName);
+// let h1 = document.getElementsByTagName('h1'); //
+// console.log(h1[0].clientTop);
 //
-var myName = 1034;
-myName = new Number(myName);
-console.log(typeof myName);
+// let para = document.getElementById('para'); //only returns id, rember id is for javascript. should stop using it to style
 //
-var myAge = 28;
-myAge = new Number(myAge);
-console.log('this is my age ' + myAge);
-
-var myName = 'Crystal';
-// myName = new String(myName);
-console.log(myName.toUpperCase());
-//***** Booleans *****//
-//****** Undefined *****//
-var test;
-console.log(test);
-//prints undefined
-//if you receive undefined
-//1. where were you expecting that value to come from??? which variable?
-//2. where is it actually defined, if it actually is?
+// let queryBlues = document.querySelectorAll('div'); //go back and checkout  querySelector and querySelectorAll
+// console.log(queryBlues);
 //
-//****** Null *****//
-//null is the absence of a value
-//eg it doesn't exist. how sad.
-//we use it as a developer to set a value BACK to Null
-//eg check for string, != a string, check if it's null
-//****** Block *****//
-//like a function has block code
-//objects has it's own block
-//general rules
-//{
-//a block is enclosed with curly braces.
-//blocks create scope.
-//}
-//****** var *****/
-//var test; //declared
-//test = "now this is defined";
-//one reason to do this is when you expect feedback from user, you dont know what the feedback is, so you declare a placeholder
-//in the above example, var test is the placeholder.
-console.log(myName2);
-var myName2 = 'Crystal'; //prints Undefined because javascript hoists the declartion, but doesn't hoist the definition.
+// /*
+// Let's try it out!
+//
+// Create a function that queries the DOM for the body tag using all of these methods. Log the results to your console.
+// The function should take 2 arguments: the DOM selector method the user wants to employ and the name of the DOM selector to be searched for.
+// */
+//
+// function findBody() {
+//   let body = document.getElementsByTagName('body');
+//   console.log(body[0]); //
+//   let body = document.getElementsByClassName('bodyClass');
+//   console.log((body[0]));
+//   let bodyId = document.getElementById('bodyId')
+// }
+//
+// findBody();
+//
+// function queryDom(element, method) {
+//   let elementToQuery = document.method(element);
+//   console.log(elementToQuery);
+// }
+//
+// let el = prompt('give me an element');
+// let method - prompt('give me a method');
+// queryDom(el, method);
+// event listeners
+// event listeners allow you to perform certain functionality only when user performs specific action
+// first class functions. this means functions can be given as a paramter, what is known as a
+// CALLBACK FUNCTION!!!!
+//
+// const button = document.querySelector('btn-danger');
+// //first argument is always the string name of the event you want to listen for
+// //the second argument is going to be a callback function
+// //CALLBACK FUNCTION is what happens when the whatever is clicked
+// //callback functions are run asynchronous
+// button.addEventListener('click', e => {
+//   console.log(e.target);
+// });
 
-//****** let *****/
-//let variables are NOT hoisted
-//blocked scoped. so they're basically trapped inside of the block. aka the let will stay that in the block value no matter what
-console.log(myName3);
-let myName3 = 'CrystalX'; //prints name as error because nothing is hoisted
+/*
+Let's Try It!
+On your day6 branch, create a basic HTML mockup with a button, a div with some height and background color, and an h1 with some text
+Create event listeners for each:
+For the button, alert the user with a message when they click it.
+For the div, change the color of the background when their mouse goes into the div.
+For the h1, change the color of the text the user releases their mouse.
+Not Tough Enough? Try redirecting the user's browser to facebook when the click anywhere on the page.
+*/
 
-let myName4 = 'Crystal'; //declared and defined.
-console.log(myName4, 'one');
-{
-  let myName4 = 'Bob';
-  console.log(myName4, 'two');
-}
-console.log(myName4, 'three');
+const button = document.querySelector('button');
+const container = document.querySelector('.container');
+const h1 = document.querySelctor('h1');
 
-//****** const *****/
-//const will not let you re-assign a variable. Constant.
-const myName5 = 'CrystalX';
-console.log(myName5, 'one');
+button.addEventListener('click', e => {
+  alert('thanks for the click');
+});
 
-myName5 = 'BobX'; //will throw error because can't re-defined myName5 because it's already declared using const.
-console.log(myName5, 'two');
+container.addEventListener('mouseenter', e => {
+  div.className = 'rebeccapurple';
+});
 
-console.log(myName5, 'three');
+h1.addEventListener('mouseup', e => {
+  h1.style.color = 'yellow'; //same thing as div.className, but this is better
+});
 
-//****** Object Oriented Language *****//
-//everything in javascript is an object, except the 5 data types/objects
-//eg even block of codes and global scopes are object
-
-let birthday = '11/21/1929';
-let rightNow = new Date(birthday);
-
-alert(rightNow);
-
-//****** Alert and Confrim *****//
-//alert throws up an alert, duh;
-//confirm asks for an answer which gives you boolean, true or false depending on user's answer
-//prompt has a input field to for user's answer. capture the answer using a variable.
-const userStory = prompt('tell me about your day');
-console.log(userStory);
+//DAY 7 NOTES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
