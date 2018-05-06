9cee79b24a7afd1bd8c202e394737572a7ec8a71
howdycrystalm <howdycrystalm@gmail.com>
2018-05-05 17:16:06 -0700

    finished tutorial

--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,42 @@
-// ***** Notes *****//
-// Querying the DOM
-//
-// let blueDivs = document.getElementsByClassName('blue'); //comes back as array. ELEMENTS will always be an array
-// console.log(blueDivs[0]);
-// //getElementsByClassName
-// //always give all elements
-// //always gives back an array]);
-//
-// let h1 = document.getElementsByTagName('h1'); //
-// console.log(h1[0].clientTop);
-//
-// let para = document.getElementById('para'); //only returns id, rember id is for javascript. should stop using it to style
-//
-// let queryBlues = document.querySelectorAll('div'); //go back and checkout  querySelector and querySelectorAll
-// console.log(queryBlues);
-//
-// /*
-// Let's try it out!
-//
-// Create a function that queries the DOM for the body tag using all of these methods. Log the results to your console.
-// The function should take 2 arguments: the DOM selector method the user wants to employ and the name of the DOM selector to be searched for.
-// */
-//
-// function findBody() {
-//   let body = document.getElementsByTagName('body');
-//   console.log(body[0]); //
-//   let body = document.getElementsByClassName('bodyClass');
-//   console.log((body[0]));
-//   let bodyId = document.getElementById('bodyId')
-// }
-//
-// findBody();
-//
-// function queryDom(element, method) {
-//   let elementToQuery = document.method(element);
-//   console.log(elementToQuery);
-// }
-//
-// let el = prompt('give me an element');
-// let method - prompt('give me a method');
-// queryDom(el, method);
-// event listeners
-// event listeners allow you to perform certain functionality only when user performs specific action
-// first class functions. this means functions can be given as a paramter, what is known as a
-// CALLBACK FUNCTION!!!!
-//
-// const button = document.querySelector('btn-danger');
-// //first argument is always the string name of the event you want to listen for
-// //the second argument is going to be a callback function
-// //CALLBACK FUNCTION is what happens when the whatever is clicked
-// //callback functions are run asynchronous
-// button.addEventListener('click', e => {
-//   console.log(e.target);
+// intro lesson from https://www.youtube.com/watch?v=hM9h1wN4rfU
+//Create DIV element
+// let div = document.createElement('DIV'); //usinh div as a test, and seeing what happens when div is all caps
+// // Add height to element
+// div.style.height = '100vh';
+// // Append element to the DOM
+// document.body.appendChild(div);
+// // Add an event listener
+// div.addEventListener('mousemove', function(e) {
+//   console.log(e);
+//   var x = event.clientX; //x coordinates from mousemove
+//   var y = event.clientY; //y coordinates from mousemove
+//   div.textContent = x + ', ' + y;
+//   div.style.backgroundColor = 'rgb(' + x + ', ' + y + ', 100)';
 // });
 
-/*
-Let's Try It!
-On your day6 branch, create a basic HTML mockup with a button, a div with some height and background color, and an h1 with some text
-Create event listeners for each:
-For the button, alert the user with a message when they click it.
-For the div, change the color of the background when their mouse goes into the div.
-For the h1, change the color of the text the user releases their mouse.
-Not Tough Enough? Try redirecting the user's browser to facebook when the click anywhere on the page.
-*/
+//Refctor code - make the above code more felxible and functional
 
-// const button = document.querySelector('button');
-// const container = document.querySelector('.container');
-// const h1 = document.querySelctor('h1');
-//
-// button.addEventListener('click', e => {
-//   alert('thanks for the click');
-// });
-//
-// container.addEventListener('mouseenter', e => {
-//   div.className = 'rebeccapurple';
-// });
-//
-// h1.addEventListener('mouseup', e => {
-//   h1.style.color = 'yellow'; //same thing as div.className, but this is better
-// });
+// Create DIV Element
+function fullScreen(element) {
+  let newElement = document.createElement(element);
+  newElement.style.height = '100vh';
+  document.body.appendChild(newElement);
+  return newElement;
+}
 
-//RYEKER'S NOTES FROM DAY 7
-// const div = document.querySelector('div');
-// const p = document.querySelector('p');
-// const button = document.querySelector('.btn-default');
-//
-// div.addEventListener('mouseover', (e) => {
-//   let newPTag = document.createElement('p'); //Create a new element
-//   newPTag.textContent = "Hey there, guy/gal/friend/frienemy!!!"; //add some text content to that element
-//   div.appendChild(newPTag); //add that element to another parent element
-// });
-//
-// button.addEventListener('click', (e) => {
-//   let newDiv = document.createElement('div'); //create a DIV
-//   newDiv.innerHTML = '<h3 class="text-center">Whoa! You clicked the thing!</h3><p>Some other things you might be interested in....</p>'; //Add some HTML content to the DIV you made
-//   div.insertBefore(newDiv, p); //Add you new DIV to a parent (const div), BEFORE a sibling node (const p).
-// });
-////////////////////////////DAY 8 continuation of Day 7
+// create a new function to pass to the event addEventListener
+
+function input(inputType, DOMElement, callback) {
+  DOMElement.addEventListener(inputType, function(e) {
+    var x = event.clientX; //x coordinates from mousemove
+    var y = event.clientY; //y coordinates from mousemove
+    callback(DOMElement, x, y);
+  });
+}
+
+function output(element, x, y) {
+  element.textContent = x + ', ' + y;
+  element.style.backgroundColor = 'rgb(' + x + ', ' + y + ', 100)';
+}
+
+input('mousemove', fullScreen('DIV'), output);
